9e78143bfff9ba9d910907b4ce5d21240657f962
Rock Gu <rockgu@Rocks-MacBook-Pro.local>
2019-02-12 22:04:59 -0800

    finish tableau

--- graph.js
+++ graph.js
@@ -1,4 +1,4 @@
-d3.csv("http://localhost:3000/incident_category.csv",function(error,csvdata){
+d3.csv("/incident_category.csv",function(error,csvdata){
 				var str = d3.csv.format( csvdata );
 				console.log(str.length);
 				console.log(str);
