7c09df27ae1c1673d298d64d39b6619869fa69a3
Rock Gu <rockgu@Rocks-MacBook-Pro.local>
2019-02-13 22:50:18 -0800

    final version

--- graph.js
+++ graph.js
@@ -1,5 +1,40 @@
-d3.csv("/incident_category.csv",function(error,csvdata){
-				var str = d3.csv.format( csvdata );
-				console.log(str.length);
-				console.log(str);
-			});
+var dataset = [80, 200, 56, 120, 180, 30, 40, 120, 160];
+//var dataset1 = ["ad","b","c","h","g,","40","40","120","160"]
+
+var svgWidth = 500, svgHeight = 500, barPadding = 5;
+var barWidth = svgWidth / dataset.length;
+var svg = d3.select('svg')
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+var barChart = svg.selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("y", function(d) {
+         return svgHeight - d
+    })
+    .attr("height", function(d) {
+        return d;
+    })
+    .attr("width", barWidth - barPadding)
+    .attr("class", "bar")
+    .attr("transform", function (d, i) {
+        var translate = [barWidth * i, 0];
+        return "translate("+ translate +")";
+    });
+
+var text = svg.selectAll("text")
+    .data(dataset)
+    .enter()
+    .append("text")
+    .text(function(d) {
+        return d;
+    })
+    .attr("y", function(d, i) {
+        return svgHeight - d - 2;
+    })
+    .attr("x", function(d, i) {
+        return barWidth * i;
+    })
+    .attr("fill", "#A64C38");
