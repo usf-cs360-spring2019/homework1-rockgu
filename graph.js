d7833e003415797310806df233cb7d544d523f28
Rock Gu <you@example.com>
2019-02-14 21:23:05 -0800

    final

--- graph.js
+++ graph.js
@@ -1,40 +1,82 @@
-var dataset = [80, 200, 56, 120, 180, 30, 40, 120, 160];
-//var dataset1 = ["ad","b","c","h","g,","40","40","120","160"]
+var margin2 = {top2: 100, right2: 20, bottom2: 50, left2: 200},
+          width2 = 960 - margin2.left2 - margin2.right2,
+          height2 = 500 - margin2.top2 - margin2.bottom2;
 
-var svgWidth = 500, svgHeight = 500, barPadding = 5;
-var barWidth = svgWidth / dataset.length;
-var svg = d3.select('svg')
-    .attr("width", svgWidth)
-    .attr("height", svgHeight);
+        var svg2 = d3.select("body").append("svg")
+          .attr("width", width2 + margin2.left2 + margin2.right2)
+          .attr("height", height2 + margin2.top2 + margin2.bottom2)
+          .append("g")
+          .attr("transform","translate(" + margin2.left2 + "," + margin2.top2 + ")");
+          d3.csv("incident.csv").then(function(data2) {
+            console.log(data2);
+            data2.forEach(function(d) {
+              d.Count = +d.Count;
+            });
+            let days = data2.map(function(d){return d.Day});
+            let count = data2.map(function(d){return d.Count});
 
-var barChart = svg.selectAll("rect")
-    .data(dataset)
-    .enter()
-    .append("rect")
-    .attr("y", function(d) {
-         return svgHeight - d
-    })
-    .attr("height", function(d) {
-        return d;
-    })
-    .attr("width", barWidth - barPadding)
-    .attr("class", "bar")
-    .attr("transform", function (d, i) {
-        var translate = [barWidth * i, 0];
-        return "translate("+ translate +")";
-    });
+            console.log(days)
+            console.log(count)
 
-var text = svg.selectAll("text")
-    .data(dataset)
-    .enter()
-    .append("text")
-    .text(function(d) {
-        return d;
-    })
-    .attr("y", function(d, i) {
-        return svgHeight - d - 2;
-    })
-    .attr("x", function(d, i) {
-        return barWidth * i;
-    })
-    .attr("fill", "#A64C38");
+            // the mapping from data to pixel values
+            var y2 = d3.scaleBand()
+                    .domain(days)
+                    .range([height2, 0])
+                    .padding(0.2);
+            var x2 = d3.scaleLinear()
+                    .domain([0, d3.max(count)])
+                    .range([0, width2]);
+
+            // append the rectangles for the bar chart
+            console.log(data2)
+            svg2.selectAll(".bar2")
+                .data(data2)
+                .enter().append("rect")
+                .attr("class", "bar")
+                .attr("width", function(d) {return x2(d.Count); })
+                .attr("y", function(d) { return y2(d.Day); })
+                .attr("height", y2.bandwidth());
+
+            svg2.selectAll("rect")
+                .data([[78,121,167], [78,121,167], [78,121,167], [78,121,167], [78,121,167], [78,121,167], [78,121,167], [78,121,167], [78,121,167], [78,121,167]])
+                .style("fill", function (d, i) {
+                  return "rgb(" + d[0] + ", " + d[1] + ", " + d[2] + ")";
+                });
+
+
+              // gridlines in x axis function
+              function make_x_gridlines() {
+                  return d3.axisBottom(x2)
+                      .ticks(15)
+              }
+
+            // add the y Axis
+            svg2.append("g")
+                .call(d3.axisLeft(y2));
+
+            // add the x Axis
+            svg2.append("g")
+                .attr("transform", "translate(0," + height2 + ")")
+                .call(d3.axisBottom(x2));
+
+            svg2.append("text")
+                .attr("font-size", "23px")
+                .attr("transform","translate(" + (width2-700) + " ," + (-70) + ")")
+                .style("text-anchor", "right")
+                .text("Top 10 crime in San Francisco")
+            svg2.append("text")
+                .attr("transform","translate(" + (width2-870) + " ," + (-20) + ")")
+                .style("text-anchor", "front")
+                .text("Incident of Category");
+
+            svg2.append("text")
+                .attr("transform","translate(" + (width2/2-150) + " ," + (margin2.bottom2+350) + ")")
+                .style("text-anchor", "front")
+                .text("Count of times");
+
+            // gridlines in x axis function
+            function make_x_gridlines() {
+                return d3.axisBottom(x2)
+                    .ticks(15)
+                }
+          });
--- graph2.js
+++ graph2.js
@@ -1,45 +1,83 @@
-var data = [
-  {"platform": "Friday", "percentage": 40.11},
-   {"platform": "Tuesday", "percentage": 36.69},
-   {"platform": "Wednesday", "percentage": 13.06}
+var margin = {top: 60, right: 20, bottom: 60, left: 200},
+          width = 960 - margin.left - margin.right,
+          height = 500 - margin.top - margin.bottom;
+      // set the ranges
+      var x = d3.scaleBand()
+                .range([0, width])
+                .padding(0.1);
+      var y = d3.scaleLinear()
+                .range([height, 0]);
+      // append the svg object to the body of the page
+      // append a 'group' element to 'svg'
+      // moves the 'group' element to the top left margin
+      var svg = d3.select("body").append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+          .append("g")
+          .attr("transform",
+                "translate(" + margin.left + "," + margin.top + ")");
+      // gridlines in y axis function
+      function make_y_gridlines() {
+          return d3.axisLeft(y)
+              .ticks(8)
+      }
+      // get the data
+      d3.csv("book1.csv").then(function(data) {
+        console.log(data);
+        data.forEach(function(d) {
+          d.Count = +d.Count;
+        });
+        // format the data
+        let hours = Array.from(data.keys())
+        let count = data.map(function(d){return d.Count})
+        console.log(data.map(function(d){return d.Count}))
 
-];
+        // Scale the range of the data in the domains
+        x.domain(hours);
+        y.domain([0, d3.max(count)]);
+        console.log(d3.max(count.map(function(d) { return d.Count; })))
+        // append the rectangles for the bar chart
+        svg.selectAll(".bar")
+          .data(data)
+          .enter().append("rect")
+          .attr("class", "bar")
+          .attr("x", function(d) { return x(d.Hour); })
+          .attr("width", x.bandwidth())
+          .attr("y", function(d) { return y(d.Count); })
+          .attr("height", function(d) { return height - y(d.Count); });
 
-var svgWidth = 500, svgHeight = 300, radius =  Math.min(svgWidth, svgHeight) / 2;
-var svg = d3.select('svg')
-    .attr("width", svgWidth)
-    .attr("height", svgHeight);
+        // add the x Axis
+        svg.append("g")
+          .attr("transform", "translate(0," + height  + ")")
+          .call(d3.axisBottom(x));
 
-//Create group element to hold pie chart
-var g = svg.append("g")
-    .attr("transform", "translate(" + radius + "," + radius + ")") ;
+        // add the y Axis
+        svg.append("g")
+          .call(d3.axisLeft(y));
 
-var color = d3.scaleOrdinal(d3.schemeCategory10);
+        // add the Y gridlines
+        svg.append("g")
+            .attr("class", "grid")
+            .call(make_y_gridlines()
+                .tickSize(-width)
+                .tickFormat(""))
 
-var pie = d3.pie().value(function(d) {
-     return d.percentage;
-});
+        svg.append("text")
+          .attr("font-size", "25px")
+          .attr("transform","translate(" + (width-600) + " ," + (margin.top-100) + ")")
+          .style("text-anchor", "right")
+          .text("Larceny Theft Frequency in San Francisco")
+      });
 
-var path = d3.arc()
-    .outerRadius(radius)
-    .innerRadius(0);
+      svg.append("text")
+         .attr("transform", "rotate(-90)")
+         .attr("y", 150 - margin.left)
+         .attr("x",0 - (height / 2))
+         .attr("dy", "1em")
+         .style("text-anchor", "middle")
+         .text("Count of Incident Number");
 
-var arc = g.selectAll("arc")
-    .data(pie(data))
-    .enter()
-    .append("g");
-
-arc.append("path")
-    .attr("d", path)
-    .attr("fill", function(d) { return color(d.data.percentage); });
-
-var label = d3.arc()
-    .outerRadius(radius)
-    .innerRadius(0);
-
-arc.append("text")
-    .attr("transform", function(d) {
-        return "translate(" + label.centroid(d) + ")";
-    })
-    .attr("text-anchor", "middle")
-    .text(function(d) { return d.data.platform+":"+d.data.percentage+"%"; });
+       svg.append("text")
+         .attr("transform","translate(" + (width/2) + " ," + (margin.top-70) + ")")
+         .style("text-anchor", "middle")
+         .text("Incident Time");
